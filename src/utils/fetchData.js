64d6c30002146431c2710fb1f1ede670e380d8a2
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-10-05 03:17:05 -0400

    bleh

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,8 +3,6 @@ const userId = "848339671629299742";
 export async function fetchUserData() {
     const userData = {
         badges: [],
-        status: "",
-        avatar: "",
         clanName: "",
         clanBadge: "",
         customStatus: "",
@@ -16,6 +14,30 @@ export async function fetchUserData() {
     };
 
     try {
+
+        const lanyardRes = await fetch(`https://lanyard.atums.world/v1/users/${userId}`);
+        if (lanyardRes.ok) {
+            const lanyardJson = await lanyardRes.json();
+            const lanyardData = lanyardJson?.data;
+            const user = lanyardData?.discord_user;
+            const clan = user?.primary_guild;
+            const clanBadge = `https://cdn.discordapp.com/clan-badges/${clan?.identity_guild_id}/${clan?.badge}.png?size=16`
+            const customStatus = lanyardData?.activities?.find((a) => a.type === 4);
+            const activity = lanyardData?.activities?.find((a) => a.type !== 4);
+
+            userData.clanBadge = clanBadge;
+            userData.clanName = clan?.tag;
+
+            userData.customStatus = customStatus?.state?.slice(0, 10) + "...";
+            userData.activity = activity?.details ?? activity?.name;
+
+            userData.active_on_discord_web = lanyardData?.active_on_discord_web;
+            userData.active_on_discord_mobile = lanyardData?.active_on_discord_mobile;
+            userData.active_on_discord_desktop = lanyardData?.active_on_discord_desktop;
+            userData.active_on_discord_embedded = lanyardData?.active_on_discord_embedded;
+
+        }
+
         const badgeRes = await fetch(`https://badges.equicord.org/${userId}?seperated=true&capitalize=true`);
         if (badgeRes.ok) {
             const badgeData = await badgeRes.json();
@@ -38,28 +60,6 @@ export async function fetchUserData() {
             }
         }
 
-        const lanyardRes = await fetch(`https://lanyard.equicord.org/v1/users/${userId}`);
-        if (lanyardRes.ok) {
-            const lanyardJson = await lanyardRes.json();
-            const lanyardData = lanyardJson?.data;
-            const user = lanyardData?.discord_user;
-            const clan = user?.primary_guild;
-            const clanBadge = `https://cdn.discordapp.com/clan-badges/${clan?.identity_guild_id}/${clan?.badge}.png?size=16`
-            const customStatus = lanyardData?.activities?.find((a) => a.type === 4);
-            const activity = lanyardData?.activities?.find((a) => a.type !== 4);
-
-            userData.clanBadge = clanBadge;
-            userData.clanName = clan?.tag;
-
-            userData.customStatus = customStatus?.state?.slice(0, 10) + "...";
-            userData.activity = activity?.details ?? activity?.name;
-
-            userData.active_on_discord_web = lanyardData?.active_on_discord_web;
-            userData.active_on_discord_mobile = lanyardData?.active_on_discord_mobile;
-            userData.active_on_discord_desktop = lanyardData?.active_on_discord_desktop;
-            userData.active_on_discord_embedded = lanyardData?.active_on_discord_embedded;
-        }
-
         return userData;
     } catch (error) {
         console.error("Error fetching user data:", error);
@@ -71,6 +71,7 @@ export async function fetchUserStatus() {
     const userData = {
         status: "",
         avatar: "",
+        decor: "",
     };
 
     try {
@@ -80,9 +81,23 @@ export async function fetchUserStatus() {
             const lanyardData = lanyardJson?.data;
             const user = lanyardData?.discord_user;
             const avatar = `https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}.png?size=240`
+            const decorationData = user?.avatar_decoration_data;
+            const decor = `https://cdn.discordapp.com/avatar-decoration-presets/${decorationData?.asset}.png?size=64&passthrough=true`;
 
             userData.status = lanyardData?.discord_status || "offline";
             userData.avatar = avatar;
+
+            if (decorationData) {
+                userData.decor = decor;
+            } else {
+                const decorApi = await fetch(`https://decor.fieryflames.dev/api/users/${userId}`);
+                if (decorApi.ok) {
+                    const decorData = await decorApi.json();
+                    if (decorData.decorationHash) {
+                        userData.decor = `https://ugc.decor.fieryflames.dev/${decorData.decorationHash}.png?animated=true`;
+                    }
+                }
+            }
         }
 
         return userData;
